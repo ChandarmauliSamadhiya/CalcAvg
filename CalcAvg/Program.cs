86af9a0dda2a4a08a4d16c914e3aa99320d32154
acer <acer@ASUR>
2023-08-13 21:22:01 +0530

    Add project files.

---
+++ CalcAvg/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcAvg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] values = { 2.5, 3.7, 1.2, 4.6, 5.9 };
+            double sum = 0;
+
+            foreach (double val in values)
+            {
+                sum += val;
+            }
+
+            double average = sum / values.Length;
+
+            Console.WriteLine("Average: " + average);
+            Console.ReadLine();
+        }
+    }
+}
